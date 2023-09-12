626b37db52ad24f66ee9096b60aa4fc9ad269502
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2023-09-12 15:23:32 +0300

    variables and print message

---
+++ hello.py
@@ -0,0 +1,4 @@
+#in this case first_name and surname acts as a variable storing the user's name
+first_name=input("Please enter your name: ")
+surname=input("enter your surname: ")
+print("Hello ",first_name,surname," Welcome!")
