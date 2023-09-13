19593dd5dd47378157646a56ee0a811a422d454e
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2023-09-13 08:42:44 +0300

    a program that asks the user to enter the width and length of a room.  program computes and displays the area of theroom. The length and the width are entered as ﬂoating point numbers. Included units in prompt and output message both  feet and metres

---
+++ area_of_a_room.py
@@ -0,0 +1,10 @@
+length=float(input("Enter the length of the room: "))
+width=float(input("Enter the width of the room: "))
+
+Area= length * width
+
+#AREA IN METERS
+print("Area of the room is",Area,"square metres")
+
+#AREA IN FEET
+print("Area of the room is",Area,"square feet")
